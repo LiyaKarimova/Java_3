c59545833968a9837e296f6937bdcb5681a2bdfe
liyakarimova <liyakaliyaka@gmail.com>
2022-02-07 15:08:44 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/lesson1/MainApp.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.lesson1;
+
+import java.util.ArrayList;
+
+public class MainApp {
+
+
+    public static ArrayList changePlace (ArrayList arr) {
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        ArrayList arr = new ArrayList<>(1,2,46,4,7);
+        changePlace (arr);
+    }
+
+
+}
+
+
+
